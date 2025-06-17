87d0627738e967e8c803094f5e4b58f3728f3573
AfghanGoat <afghangoat@afghangoat.hu>
2025-06-17 12:58:28 +0200

    Initial commit - Mirrored from my site, file: POLYCPP/POLYCPP.h

---
+++ POLYCPP/POLYCPP.h
@@ -0,0 +1,16 @@
+//POLYCPP 1.1 by Afghan Goat
+
+#ifndef POLYCPP
+#define POLYCPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "POLYCPP_exception.h"
+#include "POLYCPP_trycatch.h"
+#include "POLYCPP_namespace.h"
+#include "POLYCPP_class.h"
+#include "POLYCPP_iterator.h"
+
+#endif
